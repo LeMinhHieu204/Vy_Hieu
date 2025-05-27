8a295058d0b53d8fe858442bafd5072ff516da1c
Tuong-Vy123 <tuongvyhuynh0410@gmail.com>
2025-05-27 08:50:01 +0700

    Thêm Form để thêm liên hệ mới

---
+++ ContactList/ContactList/AddContactForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace ContactList
+{
+    partial class AddContactForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txthoten = new System.Windows.Forms.TextBox();
+            this.txtsdt = new System.Windows.Forms.TextBox();
+            this.btnluu = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txthoten
+            // 
+            this.txthoten.Location = new System.Drawing.Point(28, 32);
+            this.txthoten.Name = "txthoten";
+            this.txthoten.Size = new System.Drawing.Size(100, 22);
+            this.txthoten.TabIndex = 0;
+            this.txthoten.Text = "Họ tên";
+            this.txthoten.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtsdt
+            // 
+            this.txtsdt.Location = new System.Drawing.Point(28, 97);
+            this.txtsdt.Name = "txtsdt";
+            this.txtsdt.Size = new System.Drawing.Size(100, 22);
+            this.txtsdt.TabIndex = 1;
+            this.txtsdt.Text = "Số điện thoại";
+            // 
+            // btnluu
+            // 
+            this.btnluu.Location = new System.Drawing.Point(137, 306);
+            this.btnluu.Name = "btnluu";
+            this.btnluu.Size = new System.Drawing.Size(75, 23);
+            this.btnluu.TabIndex = 2;
+            this.btnluu.Text = "Lưu";
+            this.btnluu.UseVisualStyleBackColor = true;
+            // 
+            // AddContactForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnluu);
+            this.Controls.Add(this.txtsdt);
+            this.Controls.Add(this.txthoten);
+            this.Name = "AddContactForm";
+            this.Text = "AddContactForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txthoten;
+        private System.Windows.Forms.TextBox txtsdt;
+        private System.Windows.Forms.Button btnluu;
+    }
+}
