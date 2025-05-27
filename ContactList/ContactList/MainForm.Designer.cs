47f5f32168a37c436a104d8205bc8e53d637a9cf
Leminhhieu204 <sadodragon1597538426@gmailcom>
2025-05-27 08:38:44 +0700

    Khởi tạo dự án Windows Forms với MainForm

--- ContactList/ContactList/MainForm.Designer.cs
+++ ContactList/ContactList/MainForm.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnthemlienhe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(319, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Danh sách liên hệ";
+            // 
+            // btnthemlienhe
+            // 
+            this.btnthemlienhe.Location = new System.Drawing.Point(343, 138);
+            this.btnthemlienhe.Name = "btnthemlienhe";
+            this.btnthemlienhe.Size = new System.Drawing.Size(104, 41);
+            this.btnthemlienhe.TabIndex = 1;
+            this.btnthemlienhe.Text = "Thêm liên hệ";
+            this.btnthemlienhe.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnthemlienhe);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnthemlienhe;
     }
 }
 
